945202f55fbe4efb83533cd8b1f38de0df2cc4e4
Simiyutin Boris <simiyutin.boris@yandex.ru>
2018-02-02 21:35:10 +0300

    y-flipping loaded textures

--- src/js/parallaxRenderingStrategy.js
+++ src/js/parallaxRenderingStrategy.js
@@ -127,6 +127,7 @@ class ParallaxRenderingStrategy {
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
         };
         image.src = `http://localhost:8000/textures/${name}.jpg`;
 
